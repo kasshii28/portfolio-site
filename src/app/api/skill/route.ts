c754d15d4db5e9e1dffb09820dc13b81e37a352b
kasshii28 <gp22a043@oecu.jp>
2024-03-18 19:28:10 +0900

    apiの修正

--- src/app/api/skill/route.ts
+++ src/app/api/skill/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const res = await fetch(`http://localhost:3001/skillLists`);
+  const res = await fetch(`http://localhost:3001/skillLists`,{
+    cache: "no-store",
+  });
   const skillData = await res.json();
   return NextResponse.json(skillData);
 }
