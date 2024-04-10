65e9528b4645de655f45631a63d4baa26c0063e3
kasshii28 <gp22a043@oecu.jp>
2024-04-10 10:52:51 +0900

    urlの変更

--- src/app/api/skill/route.ts
+++ src/app/api/skill/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const apiUrl = process.env.NEXT_PUBLIC_JSON_URL;
   const res = await fetch(`${apiUrl}/skillLists`,{
     cache: "no-store",
   });
+
   const skillData = await res.json();
   return NextResponse.json(skillData);
 }
